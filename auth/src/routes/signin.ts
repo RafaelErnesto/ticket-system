66cd96626f02bfce5e06a8132ba5da8843ad0465
Rafael <rflernesto@gmail.com>
2021-06-02 21:29:24 -0400

    feature: return token when sigin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, {Request, Response} from 'express'
+import jwt from 'jsonwebtoken'
+
 import { validateRequest } from '../middlewares/validate-request'
 import { body } from 'express-validator'
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
 
 const router = express.Router()
 
@@ -14,10 +19,31 @@ router.post('/api/users/signin', [
     .withMessage('You must provide a password')
 ],
 validateRequest,
+async (req: Request, res: Response) => {
+    const { email, password } = req.body
 
-(req: Request, res: Response) => {
+    const existingUser = await User.findOne({ email })
 
-    res.send('Hi There!')
+    if(!existingUser) {
+        throw new BadRequestError('Invalid credentials')
+    }
+
+    const passwordMatch = await Password.compare(existingUser.password, password)
+
+    if(!passwordMatch) {
+        throw new BadRequestError('Invalid credentials')
+    }
+
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    }, process.env.JWT_KEY!)
+
+    req.session = {
+        jwt: userJwt
+    }
+
+    return res.status(201).send(existingUser)
 
 })
 
