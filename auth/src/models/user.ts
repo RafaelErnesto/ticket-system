b0bb6a200b9e4be386191607d06142ad7003c4e3
Rafael <rflernesto@gmail.com>
2021-06-02 21:00:50 -0400

    feature: remap user model return

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -25,6 +25,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+},{
+    toJSON: {
+        transform(doc, ret) {
+            delete ret.password
+            delete ret.__v
+            ret.id  = ret._id
+            delete ret._id
+        }
+    }
 })
 
 userSchema.pre('save', async function(done) {
