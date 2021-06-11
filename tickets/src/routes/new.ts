98ac8ec6433f53c9f50e9b9eb872d5d341a8b9ac
Rafael <rflernesto@gmail.com>
2021-06-10 20:42:08 -0400

    feature: create ticket route

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,14 +1,27 @@
 import express, { Request, Response } from 'express'
 import { requireAuth, validateRequest } from '@rfltickets/common'
 import { body } from 'express-validator'
+import { Ticket } from '../models/ticket'
 
 const router = express.Router()
 
 router.post('/api/tickets', requireAuth, [
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price').isFloat({ gt: 0 }).withMessage('Price is required and must be greater than 0')
-], validateRequest, (req: Request, res: Response) => {
-    res.status(200).send({})
+], validateRequest, async (req: Request, res: Response) => {
+
+    const { title, price}  = req.body
+
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    })
+
+    await ticket.save()
+
+    
+    res.status(201).send(ticket)
 })
 
 export { router as newRouter }
