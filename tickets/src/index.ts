dba370ddc3d695dd94117d06d1ae2fb871903253
Rafael <rflernesto@gmail.com>
2021-06-27 22:24:39 -0400

    feature: add env variables to ticket deployment

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,8 +8,18 @@ const start = async () => {
         throw new Error('JWT_KEY is not defined')
     }
 
+    if(!process.env.NATS_URL) {
+        throw new Error('NATS_URL is not defined')
+    }
+    if(!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NATS_CLUSTER_ID is not defined')
+    }
+    if(!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID is not defined')
+    }
+
     try {
-        await natsWrapper.connect('ticketing', 'asd', 'http://nats-srv:4222')
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed')
             process.exit()
