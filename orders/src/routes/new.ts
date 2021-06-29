6e8d156afe7c58c7e2301f75046692961e9edbdb
Rafael <rflernesto@gmail.com>
2021-06-28 20:37:17 -0400

    feature: adds validation to order creation route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,15 @@
 import { Router, Request, Response } from 'express'
+import {requireAuth, validateRequest} from '@rfltickets/common'
+import { body } from 'express-validator'
 
 const router = Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+    body('ticketId')
+    .not()
+    .isEmpty()
+    .withMessage('TicketId must be provided')
+], async (req: Request, res: Response) => {
     res.send({})
 })
 
