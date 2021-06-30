474827ddaa388de7f2d94b07048385f8003200c1
Rafael <rflernesto@gmail.com>
2021-06-29 20:51:19 -0400

    feature: get ticket from database and check if is already reserved

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express'
-import {requireAuth, validateRequest} from '@rfltickets/common'
+import {BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest} from '@rfltickets/common'
 import { body } from 'express-validator'
+import { Ticket } from '../models/ticket'
+import { Order } from '../models/order'
 
 const router = Router()
 
@@ -10,6 +12,28 @@ router.post('/api/orders', requireAuth, [
     .isEmpty()
     .withMessage('TicketId must be provided')
 ], async (req: Request, res: Response) => {
+    const { ticketId } = req.body
+
+    const ticket = await Ticket.findById(ticketId)
+    if(!ticket) {
+        throw new NotFoundError()
+    }
+
+    const existingOrder  = await Order.findOne({
+        ticket: ticket,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete
+            ]
+        }
+    })
+
+    if(existingOrder) {
+        throw new BadRequestError('Ticket already reserved')
+    }
+
     res.send({})
 })
 
